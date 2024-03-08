b1f4804e66248f364220e4ee22b98bdd2f9aa259
Taisei <taisei96.web@gmail.com>
2024-03-09 08:36:22 +0900

    月の収支を表示するコンポーネントのUI

--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+import React from "react";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 
 const MonthlySummary = () => {
   return (
-    <div>MonthlySummary</div>
-  )
-}
+    <Grid container spacing={{ xs: 1, sm: 2 }} mb={2}>
+      {/* 収入 */}
+      <Grid item xs={4} display={"flex"} flex-direction={"column"}>
+        <Card
+          sx={{
+            bgcolor: "blue",
+            color: "white",
+            borderRadius: "10px",
+            flexGrow: 1,
+          }}
+        >
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <ArrowUpwardIcon sx={{ fontSize: "2rem" }} />
+              <Typography>収入</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"fontWeightBold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300¥300¥300¥300¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
 
-export default MonthlySummary
+      {/* 支出 */}
+      <Grid item xs={4} display={"flex"} flex-direction={"column"}>
+        <Card
+          sx={{
+            bgcolor: "red",
+            color: "white",
+            borderRadius: "10px",
+            flexGrow: 1,
+          }}
+        >
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <ArrowDownwardIcon sx={{ fontSize: "2rem" }} />
+              <Typography>支出</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"fontWeightBold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      {/* 残高 */}
+      <Grid item xs={4} display={"flex"} flex-direction={"column"}>
+        <Card
+          sx={{
+            bgcolor: "green",
+            color: "white",
+            borderRadius: "10px",
+            flexGrow: 1,
+          }}
+        >
+          <CardContent sx={{ padding: { xs: 1, sm: 2 } }}>
+            <Stack direction={"row"}>
+              <AccountBalanceIcon sx={{ fontSize: "2rem" }} />
+              <Typography>残高</Typography>
+            </Stack>
+            <Typography
+              textAlign={"right"}
+              variant="h5"
+              fontWeight={"fontWeightBold"}
+              sx={{
+                wordBreak: "break-word",
+                fontSize: { xs: ".8rem", sm: "1rem", md: "1.2rem" },
+              }}
+            >
+              ¥300
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MonthlySummary;
