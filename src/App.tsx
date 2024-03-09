eb25088c0edc2af6753feadc0366d6f0b4897470
Taisei <taisei96.web@gmail.com>
2024-03-09 17:09:00 +0900

    TSの型ガードでFirebaseのエラーを判断

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,12 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from './firebase';
 
 function App() {
+
+  // Firestoreエラーかどうかを判定するガード
+  function isFireStoreError(err: unknown):err is {code: string, message: string} {
+    return typeof err === "object" && err !== null && "code" in err
+  }
+
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   
   useEffect(() => {
@@ -20,7 +26,6 @@ function App() {
     const fecheTransactions = async() => {
       try {
         const querySnapshot = await getDocs(collection(db, "Transactions"));
-        console.log(querySnapshot);
 
         const transactionsData = querySnapshot.docs.map((doc) => {
           // doc.data() is never undefined for query doc snapshots
@@ -33,7 +38,13 @@ function App() {
         });
         setTransactions(transactionsData)
       } catch (err) {
-        // error
+        if (isFireStoreError(err)) {
+          console.error(err)
+          console.error(err.message)
+          console.error(err.code)
+        } else {
+          console.error("一般的なエラーは:",err)
+        }
       }
     }
     fecheTransactions();
