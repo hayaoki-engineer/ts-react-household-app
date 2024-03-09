5517e6bf2141215350451655cdec7da2e3ce86c7
Taisei <taisei96.web@gmail.com>
2024-03-09 16:20:44 +0900

    FireStoreからデータを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
@@ -8,8 +8,38 @@ import AppLayout from './components/layout/AppLayout';
 import {theme} from './theme/theme'
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { Transaction } from './types';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from './firebase';
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  
+  useEffect(() => {
+
+    const fecheTransactions = async() => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        console.log(querySnapshot);
+
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, " => ", doc.data());
+
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction
+        });
+        setTransactions(transactionsData)
+      } catch (err) {
+        // error
+      }
+    }
+    fecheTransactions();
+
+  },[])
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
