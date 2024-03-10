55429a789e67c30fbe86236fa21c9d05cbf04faa
Taisei <taisei96.web@gmail.com>
2024-03-11 04:23:43 +0900

    カレンダーの日付に収支 を表示

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,8 +1,40 @@
+import FullCalendar from '@fullcalendar/react'
 import React from 'react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import jaLocale from '@fullcalendar/core/locales/ja'
+import "../calender.css"
+import { EventContentArg } from '@fullcalendar/core'
 
 const Calender = () => {
+  const events = [
+    { title: 'Meeting', start: "2024-03-11", income: 300, expense: 200, balance: 100 }
+  ]
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className='money' id='event-income'>
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className='money' id='event-expense'>
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className='money' id='event-balance'>
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>Calender</div>
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView='dayGridMonth'
+      events={events}
+      eventContent={renderEventContent}
+    />
   )
 }
 
